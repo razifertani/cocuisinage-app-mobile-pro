c39e84b238b438d5ea9d08ede2b4c1ba376255db
chhamed <hamedchamkhii@gmail.com>
2022-12-19 15:20:24 +0100

    first_commit

--- lib/ui/screens/reservation/ajout_res/information.dart
+++ lib/ui/screens/reservation/ajout_res/information.dart
@@ -1,39 +1,115 @@
+import 'package:argon_buttons_flutter/argon_buttons_flutter.dart';
+import 'package:cocuisinage_app_mobile_pro_mobile_pro/ui/shared/custom_button.dart';
 import 'package:cocuisinage_app_mobile_pro_mobile_pro/ui/shared/pre_text_form.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../Theme/my_text_styles.dart';
+import '../../../../services/reservations_api.dart';
+import '../../../../utils/utils.dart';
 import '../../../shared/widgets/custom_card_text_form.dart';
+import '../../../shared/widgets/success_show_model/success_show_model.dart';
 
-class InformationsScreen extends StatelessWidget {
-  InformationsScreen({Key? key}) : super(key: key);
+typedef void InfoCallback(Map infoMap);
+
+class InformationsScreen extends StatefulWidget {
+  final InfoCallback onInfoChanged;
+  final GlobalKey globalKey;
+  InformationsScreen(
+      {Key? key, required this.onInfoChanged, required this.globalKey})
+      : super(key: key);
+
+  @override
+  State<InformationsScreen> createState() => _InformationsScreenState();
+}
+
+class _InformationsScreenState extends State<InformationsScreen> {
+  // GlobalKey<FormState> key = GlobalKey<FormState>();
 
   TextEditingController nom = TextEditingController();
-  TextEditingController prenom = TextEditingController();
-  TextEditingController email = TextEditingController();
+
   TextEditingController tlph = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          PreTextForm(txt: "Nom"),
-          CustomCardTextForm(controller: nom, hintText: "Nom"),
-          PreTextForm(txt: "Prénom"),
-          CustomCardTextForm(controller: prenom, hintText: "Prénom"),
-          PreTextForm(txt: "Email"),
-          CustomCardTextForm(
-            controller: email,
-            hintText: "Email",
-            textInputType: TextInputType.emailAddress,
-          ),
-          PreTextForm(txt: "Numéro de téléphone"),
-          CustomCardTextForm(
-            controller: tlph,
-            hintText: "Numéro de téléphone",
-            textInputType: TextInputType.phone,
-          ),
-        ],
+      child: Form(
+        key: widget.globalKey,
+        onChanged: () {
+          widget.onInfoChanged({"nom": nom.text, "nmrTlph": tlph.text});
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PreTextForm(txt: "Nom"),
+            CustomCardTextForm(
+              controller: nom,
+              hintText: "nom",
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Nom est requis !';
+                }
+                return null;
+              },
+            ),
+
+            PreTextForm(txt: "Numéro de téléphone"),
+            CustomCardTextForm(
+              controller: tlph,
+              hintText: "01.02.03.04.05",
+              textInputType: TextInputType.phone,
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Le numéro de téléphone est requis !';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            // Center(
+            //   child: CustomButton(
+            //     txt: "Réserver",
+            //     fun: (startLoading, stopLoading, btnState) {
+            //       widget.onInfoChanged({
+            //         "nom": nom.text,
+            //         "prenom": prenom.text,
+            //         "nmrTlph": tlph.text
+            //       });
+            //       if (btnState == ButtonState.Idle) {
+            //         if (key.currentState!.validate()) {
+            //           startLoading();
+            //           addReservationWS(
+
+            //             clientName: nom.text,
+            //             clientPhoneNumber: tlph.text,
+
+            //           ).then(
+            //             (exceptionOrMessage) {
+            //               stopLoading();
+            //               exceptionOrMessage.fold(
+            //                 (exception) {
+            //                   Utils.showCustomTopSnackBar(context, success: false, message: exception.toString());
+            //                 },
+            //                 (message) {
+            //                   setState(() {});
+            //                   Utils.showCustomTopSnackBar(context, success: true, message: message);
+            //                   Navigator.pop(context);
+            //                 },
+            //               );
+            //             },
+            //           );
+            //         }
+            //       }
+            //     },
+            //   ),
+            // ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/screens/reservation/ajout_res/main_screen.dart
+++ lib/ui/screens/reservation/ajout_res/main_screen.dart
@@ -1,22 +1,35 @@
+import 'package:argon_buttons_flutter/argon_buttons_flutter.dart';
 import 'package:cocuisinage_app_mobile_pro_mobile_pro/Theme/my_text_styles.dart';
 import 'package:cocuisinage_app_mobile_pro_mobile_pro/ui/screens/reservation/ajout_res/personees.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../services/reservations_api.dart';
 import '../../../shared/custom_button.dart';
-
+import '../../../../services/reservations_api.dart';
+import '../../../../utils/utils.dart';
 import '../../../shared/widgets/success_show_model/success_show_model.dart';
 import 'date.dart';
 import 'heure.dart';
 import 'information.dart';
 
 class MainResScreen extends StatefulWidget {
-  MainResScreen({Key? key}) : super(key: key);
+  final DateTime resDate;
+  final Function function;
+  MainResScreen({Key? key, required this.resDate, required this.function})
+      : super(key: key);
 
   @override
   State<MainResScreen> createState() => _MainResScreenState();
 }
 
 class _MainResScreenState extends State<MainResScreen> {
+  late DateTime date;
+  @override
+  void initState() {
+    date = widget.resDate;
+    super.initState();
+  }
+
   void switchIndex(int index) {
     currentIndex = index;
     setState(() {});
@@ -30,6 +43,30 @@ class _MainResScreenState extends State<MainResScreen> {
     "assets/icons/information.png"
   ];
   List<String> labels = ["Date", "Heure", "Personnes", "Informations"];
+
+  String timeOfDay = "8:00";
+  int? nbrPersonnes;
+
+  GlobalKey<FormState> key = GlobalKey<FormState>();
+  Map? infoMap;
+  void Valider() {}
+
+  void changeDate(DateTime date) {
+    this.date = date;
+  }
+
+  void changeHeure(String time) {
+    timeOfDay = time;
+  }
+
+  void changePersonnes(int nmbr) {
+    nbrPersonnes = nmbr;
+  }
+
+  void changeInform(Map map) {
+    infoMap = map;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,26 +119,68 @@ class _MainResScreenState extends State<MainResScreen> {
             SizedBox(
               height: 20,
             ),
-            if (currentIndex == 0) DateScreen(),
-            if (currentIndex == 1) HeureScreen(),
-            if (currentIndex == 2) PersonnesScreen(),
-            if (currentIndex == 3) InformationsScreen(),
+            if (currentIndex == 0)
+              DateScreen(
+                ondateChanged: changeDate,
+                initialDateTime: widget.resDate,
+              ),
+            if (currentIndex == 1)
+              HeureScreen(
+                ontimeChanged: changeHeure,
+              ),
+            if (currentIndex == 2)
+              PersonnesScreen(
+                onnbrChanged: changePersonnes,
+              ),
+            if (currentIndex == 3)
+              InformationsScreen(
+                onInfoChanged: changeInform,
+                globalKey: key,
+              ),
             SizedBox(
               height: 10,
             ),
-            CustomButton(
-                txt: currentIndex == 3 ? "Réserver" : "Suivant",
-                fun: (a, b, c) {
-                  if (currentIndex == 3) {
-                    showDialog(
-                        context: context,
-                        builder: (_) => SuccessShowmodel(
-                              description: "Super! réservation asurée",
-                            ));
-                  }
-                  if (currentIndex < 3) currentIndex++;
-                  setState(() {});
-                }),
+            currentIndex <= 3
+                ? CustomButton(
+                    txt: currentIndex == 3 ? "Réserver" : "Suivant",
+                    fun: (startLoading, stopLoading, btnState) {
+                      if (currentIndex == 3) {
+                        if (btnState == ButtonState.Idle) {
+                          if (key.currentState!.validate()) {
+                            startLoading();
+                            addReservationWS(
+                              clientName: infoMap!['nom'],
+                              clientPhoneNumber: infoMap!['nmrTlph'],
+                              nbPeople: nbrPersonnes.toString(),
+                              day: date,
+                              hour: timeOfDay,
+                            ).then(
+                              (exceptionOrMessage) {
+                                stopLoading();
+                                exceptionOrMessage.fold(
+                                  (exception) {
+                                    Utils.showCustomTopSnackBar(context,
+                                        success: false,
+                                        message: exception.toString());
+                                    Navigator.pop(context);
+                                  },
+                                  (message) {
+                                    setState(() {});
+                                    Utils.showCustomTopSnackBar(context,
+                                        success: true, message: message);
+                                    widget.function();
+                                    Navigator.pop(context);
+                                  },
+                                );
+                              },
+                            );
+                          }
+                        }
+                      }
+                      if (currentIndex < 3) currentIndex++;
+                      setState(() {});
+                    })
+                : SizedBox(),
             SizedBox(
               height: 10,
             ),
